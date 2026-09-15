2c48f4272f283029c33b6bc3b10f2aa5eff83ef3
craftverzegaming-sys <craftverzegaming@gmail.com>
2026-09-15 13:03:09 +0530

    Update TextRendererMixin.java

--- src/main/java/com/craftverze/playernamexpbar/mixin/TextRendererMixin.java
+++ src/main/java/com/craftverze/playernamexpbar/mixin/TextRendererMixin.java
@@ -12,7 +12,6 @@ public class TextRendererMixin {
 
     @Inject(method = "draw(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I", at = @At("HEAD"), cancellable = true)
     private void hideVanillaXpNumber(String text, float x, float y, int color, boolean shadow, org.joml.Matrix4f matrix, net.minecraft.client.render.VertexConsumerProvider vertexConsumers, net.minecraft.client.font.TextRenderer.TextLayerType layerType, int backgroundColor, int light, boolean rightToLeft, CallbackInfoReturnable<Integer> cir) {
-        // If it's a numeric string (XP Level) and we aren't currently drawing our player name, block it
         if (!PlayerNameXPBarClient.renderingXPName && text != null && text.matches("\\d+")) {
             cir.setReturnValue(0);
         }
