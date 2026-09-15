701421c0f880babf0bd130f5b372e2fac6d4687a
craftverzegaming-sys <craftverzegaming@gmail.com>
2026-09-15 13:00:57 +0530

    Create TextRendererMixin.java

---
+++ src/main/java/com/craftverze/playernamexpbar/mixin/TextRendererMixin.java
@@ -0,0 +1,20 @@
+package com.craftverze.playernamexpbar.mixin;
+
+import com.craftverze.playernamexpbar.PlayerNameXPBarClient;
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TextRenderer.class)
+public class TextRendererMixin {
+
+    @Inject(method = "draw(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I", at = @At("HEAD"), cancellable = true)
+    private void hideVanillaXpNumber(String text, float x, float y, int color, boolean shadow, org.joml.Matrix4f matrix, net.minecraft.client.render.VertexConsumerProvider vertexConsumers, net.minecraft.client.font.TextRenderer.TextLayerType layerType, int backgroundColor, int light, boolean rightToLeft, CallbackInfoReturnable<Integer> cir) {
+        // If it's a numeric string (XP Level) and we aren't currently drawing our player name, block it
+        if (!PlayerNameXPBarClient.renderingXPName && text != null && text.matches("\\d+")) {
+            cir.setReturnValue(0);
+        }
+    }
+}
